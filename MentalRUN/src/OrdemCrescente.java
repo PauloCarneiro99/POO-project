4299ba59691143676cb34c3a2435a12898b4ef2a
Fabio F. Destro <fbfdestro@gmail.com>
2018-06-25 17:50:25 -0300

    Ordem crescente

---
+++ MentalRUN/src/OrdemCrescente.java
@@ -0,0 +1,109 @@
+import java.awt.Font;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Vector;
+
+import javax.xml.bind.ParseConversionEvent;
+
+public class OrdemCrescente extends BaseJogos {
+
+	private Random r = null;
+	private Vector<Integer> numeros = new Vector<Integer>();
+	private HashSet<Integer> foi  = new HashSet<Integer>();
+	private int quantidade = 3, atual = 0, rodada = 0;
+	
+	
+	public OrdemCrescente() {
+		super("Ordem crescente", "Clice nos números na ordem crescente");
+		
+		
+		r = new Random();//criando o rand
+		montaTabuleiro();
+	}
+
+	@Override
+	String oqTemnoBotao(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	void clicouBotao(int i) {
+		// TODO Auto-generated method stub
+		
+		int num = Integer.parseInt(botoes.elementAt(i).getText());
+		
+		if(numeros.elementAt(atual) != num){ // errado
+			penalidade(3);
+			montaTabuleiro();
+		}else {
+			botoes.elementAt(i).setVisible(false);
+			atual++;
+			if(atual == quantidade){
+				int aumento = r.getIntRandom(1,4);
+				quantidade+=aumento;
+				rodada++;
+				if(rodada == 5){
+					finaliza();
+				}
+				montaTabuleiro();
+			}
+		}
+		
+	}
+	
+	private void limpaTabuleiro(){		
+		for(int i = 0; i < 42; i++){
+			botoes.elementAt(i).setText("");
+			botoes.elementAt(i).setVisible(false);
+		}
+	}
+
+	@Override
+	void montaTabuleiro() {
+		
+		foi.clear();
+		numeros.clear();
+		atual = 0;
+		limpaTabuleiro();
+		
+		for(int i=0;i<quantidade;i++){
+			int at;
+			
+			int ini = 0 , fin = 20;
+			if(rodada >= 3){
+				ini = -9;
+				fin = 20;
+			}
+			
+			while(true){
+				at = r.getIntRandom(ini, fin);
+				if(!foi.contains(at)){ // se ainda não foi usado nesse tabuleiro 
+					foi.add(at); // adiciona
+					break;
+				}
+			}
+		
+			numeros.add(at);
+		}
+		
+		Collections.sort(numeros);
+		
+		for(int i=0;i<quantidade;i++){
+			int pos;
+			while(true){
+				pos = r.getIntRandom(0,42);
+				if(!botoes.elementAt(pos).isVisible()){ // se ainda não esta setado
+					break;
+				}
+			}
+			
+			botoes.elementAt(pos).setText(numeros.elementAt(i).toString());//coloco no botao o numero
+			botoes.elementAt(pos).setForeground(cores[r.getIntRandom(4)]);
+			botoes.elementAt(pos).setVisible(true);
+			botoes.elementAt(pos).setFont(new Font("Arial", Font.PLAIN, 20));
+		}
+		
+	}
+
+}
